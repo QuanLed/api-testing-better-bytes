1617035c6c495ddf053740a5ea50b368ec5a190a
quanle1 <quanle1@beeketing.net>
2025-03-23 19:04:38 +0700

    lesson 04

---
+++ lesson-04/01-setup.js
@@ -0,0 +1,8 @@
+let missionObjective = "Khám phá và kiểm thử API";
+let crew = ["Phong", "Nga", "Quan"];
+
+console.log(
+  `Hệ thống API Explorer đã sẵn sàng! Phi hành đoàn: ${crew
+    .toString()
+    .replaceAll(",", ", ")} sẽ cùng bạn thực hiện nhiệm vụ ${missionObjective}!`
+);
---
+++ lesson-04/02-reason.js
@@ -0,0 +1,4 @@
+let apiDefinition = "API là cách thức trao đổi dữ liệu và các yêu cầu giữa các ứng dụng, hệ thống.";
+let whyTestAPI = ["Phát hiện lỗi sớm", "Tăng cường bảo mật", "Đảm bảo hiệu suất"];
+
+whyTestAPI.forEach((value,index) => {console.log(`Lý do ${index + 1}: ${value}`)});
